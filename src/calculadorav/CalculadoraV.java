75b695ac11a0479907df4046a6431e9803269c8d
Nick <Nick@192.168.0.13>
2024-05-23 05:12:08 -0300

    Updated

---
+++ src/calculadorav/CalculadoraV.java
@@ -0,0 +1,21 @@
+
+package calculadorav;
+
+
+
+public class CalculadoraV {
+
+  
+    public static void main(String[] args) {
+        Panel panel = new Panel();
+        
+       panel.setVisible(true);
+         java.awt.EventQueue.invokeLater(() -> {
+            
+  });
+        
+        
+        
+    }
+    
+}//
